188f76a7d350e06de3b97222a25155beb040c4f1
Seth <silver1row@cox.net>
2021-03-02 09:09:05 -0600

    Updating to make work...
    Just some minor improvements...

--- BeagleBone/Green/Grove/Software/Python/Grove_UV_Sensor.py
+++ BeagleBone/Green/Grove/Software/Python/Grove_UV_Sensor.py
@@ -9,6 +9,6 @@ while True:
     value = ADC.read("P9_40")
     voltage = value * 1.8 # 1.8V
     print ("method_1:", voltage, "V")
-    print ("method_2:", ADC.read_raw("AIN1"), "mV"))
+    print ("method_2:", ADC.read_raw("AIN1"), "mV")
     time.sleep(0.5)
 

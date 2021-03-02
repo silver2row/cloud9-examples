7201a045a192deca65de656d009bc4d7eefe2829
Seth <silver1row@cox.net>
2021-03-01 22:52:25 -0600

    Updates for Python3 print statements...(trying)
    I think maybe one print statement in this source might be a bit faulty. I will keep testing once I get the hardware.

--- BeagleBone/Green/Grove/Software/Python/Grove_dht.py
+++ BeagleBone/Green/Grove/Software/Python/Grove_dht.py
@@ -24,7 +24,7 @@ class DHT():
         
     def read(self):        
         laststate = 1
-        counter, j, i = 0,0,0
+        counter, j, i = 0, 0, 0
         currenttime = 0
         GPIO.setup(self._pin, GPIO.OUT)
         GPIO.output(self._pin, GPIO.HIGH) 
@@ -32,7 +32,7 @@ class DHT():
         
         currenttime = time.time()
         if currenttime < self._lastreadtime:
-            _lastreadtime=0
+            _lastreadtime = 0
         if (False == self.firstreading) and ((currenttime - self._lastreadtime) < 2.00):
             return True
         self.firstreading = False
@@ -56,13 +56,13 @@ class DHT():
             laststate = GPIO.input(self._pin)
             if counter == 255:
                 break
-            if i >= 4 and i%2 == 0:
+            if i >= 4 and i % 2 == 0:
                 self.data[j/8] <= 1
                 if counter > self._count:
-                    self.data[j/8] |= 1
+                    self.data[j / 8] |= 1
                 j += 1
-        print "data[0] = ",self.data[0]
-        if j >= 40 and self.data[4] == (self.data[0] + self.data[1] + self.data[2] + self.data[3])&0xFF:
+        print ("data[0] = ", self.data[0]())
+        if j >= 40 and self.data[4] == (self.data[0] + self.data[1] + self.data[2] + self.data[3]) & 0xFF:
             return True
         
         return False
@@ -77,14 +77,14 @@ class DHT():
                 pass
             if "DHT21" == self._type:
                 f = data[0]
-                f = f*256
-                f = f+data[1]
-                f = f/10
+                f = f * 256
+                f = f + data[1]
+                f = f / 10
                 return f
-        print 'Read fail'   
+        print ('Read fail')   
         return 0
          
-    def readTemperature(self, S=False):
+    def readTemperature(self, S = False):
         f = float()
         if self.read():
             if "DHT11" == self._type:
@@ -96,11 +96,11 @@ class DHT():
                 pass
             if "DHT21" == self._type:
                 f = data[2] & 0x7f
-                f = f*256
-                f = f+data[3]
-                f = f/10
+                f = f * 256
+                f = f + data[3]
+                f = f / 10
                 if data[2] & 0x80:
-                    f = f*(-1)
+                    f = f * (-1)
                 if S:
                     f = self.convertCtoF(f)
                 return f

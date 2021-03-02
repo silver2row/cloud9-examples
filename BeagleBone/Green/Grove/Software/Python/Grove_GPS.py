b5324473f20cf8d4a2206591bb2f4d929c5bbb51
Seth <silver1row@cox.net>
2021-03-01 22:23:42 -0600

    Python3 Updates...
    Just adding the correct print statements and spacing...for Python3.

--- BeagleBone/Green/Grove/Software/Python/Grove_GPS.py
+++ BeagleBone/Green/Grove/Software/Python/Grove_GPS.py
@@ -19,47 +19,47 @@ class GPS:
     #Read data from the GPS
     def read(self):    
         while True:
-            GPS.inp=ser.readline()
-            if GPS.inp[:6] =='$GPGGA': # GGA data , packet 1, has all the data we need
+            GPS.inp = ser.readline()
+            if GPS.inp[:6] == '$GPGGA': # GGA data , packet 1, has all the data we need
                 break
             time.sleep(0.1)
         try:
-            ind=GPS.inp.index('$GPGGA',5,len(GPS.inp))    #Sometimes multiple GPS data packets come into the stream. Take the data only after the last '$GPGGA' is seen
-            GPS.inp=GPS.inp[ind:]
+            ind = GPS.inp.index('$GPGGA', 5, len(GPS.inp))    #Sometimes multiple GPS data packets come into the stream. Take the data only after the last '$GPGGA' is seen
+            GPS.inp = GPS.inp[ind:]
         except ValueError:
-            print ""
-        GPS.GGA=GPS.inp.split(",")    #Split the stream into individual parts
+            print( "Oogle-Google!" )
+        GPS.GGA = GPS.inp.split(",")    #Split the stream into individual parts
         return [GPS.GGA]
         
     #Split the data into individual elements
     def vals(self):
-        time=GPS.GGA[1]
-        lat=GPS.GGA[2]
-        lat_ns=GPS.GGA[3]
-        long=GPS.GGA[4]
-        long_ew=GPS.GGA[5]
-        fix=GPS.GGA[6]
-        sats=GPS.GGA[7]
-        alt=GPS.GGA[9]
-        return [time,fix,sats,alt,lat,lat_ns,long,long_ew]
+        time = GPS.GGA[1]
+        lat = GPS.GGA[2]
+        lat_ns = GPS.GGA[3]
+        long = GPS.GGA[4]
+        long_ew = GPS.GGA[5]
+        fix = GPS.GGA[6]
+        sats = GPS.GGA[7]
+        alt = GPS.GGA[9]
+        return [time, fix, sats, alt, lat, lat_ns, long, long_ew]
 
-g=GPS()
-f=open("gps_data.csv",'w')    #Open file to log the data
-f.write("name,latitude,longitude\n")    #Write the header to the top of the file
-ind=0
+g = GPS()
+f = open("gps_data.csv", 'w')    #Open file to log the data
+f.write("name, latitude, longitude\n")    #Write the header to the top of the file
+ind = 0
 while True:
     try:
-        x=g.read()    #Read from GPS
-        [t,fix,sats,alt,lat,lat_ns,long,long_ew]=g.vals()    #Get the individial values
-        print "Time:",t,"Fix status:",fix,"Sats in view:",sats,"Altitude",alt,"Lat:",lat,lat_ns,"Long:",long,long_ew
+        x = g.read()    #Read from GPS
+        [t, fix, sats, alt, lat, lat_ns, long, long_ew] = g.vals()    #Get the individial values
+        print("Time:", t, "Fix status:", fix, "Sats in view:", sats, "Altitude", alt, "Lat:", lat, lat_ns, "Long:", long,long_ew)
         #s=str(t)+","+str(float(lat)/100)+","+str(float(long)/100)+"\n"    
         #f.write(s)    #Save to file
         time.sleep(2)
     except IndexError:
-        print "Unable to read"
+        print("Unable to read")
     except KeyboardInterrupt:
         f.close()
-        print "Exiting"
+        print("Exiting")
         sys.exit(0)
         
 

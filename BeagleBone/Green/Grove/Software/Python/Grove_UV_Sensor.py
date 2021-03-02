2495c55789b6be3dff8e1a58e97af26108e0ae5c
Seth <silver1row@cox.net>
2021-03-01 22:47:42 -0600

    Python3 and readability...
    Adding Python3 compatibility and readability...

--- BeagleBone/Green/Grove/Software/Python/Grove_UV_Sensor.py
+++ BeagleBone/Green/Grove/Software/Python/Grove_UV_Sensor.py
@@ -7,8 +7,8 @@ ADC.setup()
 
 while True:
     value = ADC.read("P9_40")
-    voltage = value * 1.8 #1.8V
-    print "method_1:",voltage,"V"
-    print "method_2:", ADC.read_raw("AIN1"),"mV"
+    voltage = value * 1.8 # 1.8V
+    print ("method_1:", voltage, "V")
+    print ("method_2:", ADC.read_raw("AIN1"), "mV"))
     time.sleep(0.5)
 

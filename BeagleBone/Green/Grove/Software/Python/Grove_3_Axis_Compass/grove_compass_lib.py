1e387c9c5bc1add61738958fc255dc2340e9f6f3
Seth <silver1row@cox.net>
2021-03-01 23:02:48 -0600

    Trying this lib for python3 (waiting for hardware)
    Updating to Python3 and for readability...I have not added any comments yet.

--- BeagleBone/Green/Grove/Software/Python/Grove_3_Axis_Compass/grove_compass_lib.py
+++ BeagleBone/Green/Grove/Software/Python/Grove_3_Axis_Compass/grove_compass_lib.py
@@ -31,11 +31,11 @@ import struct
 
 bus = smbus.SMBus(1)	
 
-HMC5883L_ADDRESS            =0x1E
+HMC5883L_ADDRESS            = 0x1E
 
 #CONFIGURATION_REGISTERA     =0x00
 #CONFIGURATION_REGISTERB     =0x01
-MODE_REGISTER               =0x02
+MODE_REGISTER               = 0x02
 #DATA_REGISTER_BEGIN         =0x03
 
 #MEASUREMENT_CONTINUOUS      =0x00
@@ -46,36 +46,35 @@ MODE_REGISTER               =0x02
 #http://stackoverflow.com/a/9147327/1945052
 def twos_comp(val, bits):
 	"""compute the 2's compliment of int value val"""
-	if( (val&(1<<(bits-1))) != 0 ):
+	if( (val & (1 << (bits-1))) != 0 ):
 		val = val - (1<<bits)
 	return val
 
 #Compass class for all the values and functions
 class compass:
-	x=0
-	y=0
-	z=0
-	heading=0
-	headingDegrees=0
+	x = 0
+	y = 0
+	z = 0
+	heading = 0
+	headingDegrees = 0
 	
 	def __init__(self):
 		#Enable the compass
-		bus.write_byte_data(HMC5883L_ADDRESS,MODE_REGISTER,0)
+		bus.write_byte_data(HMC5883L_ADDRESS, MODE_REGISTER, 0)
 		time.sleep(.1)
-		data=bus.read_i2c_block_data(HMC5883L_ADDRESS,0)
+		data=bus.read_i2c_block_data(HMC5883L_ADDRESS, 0)
 		compass.update(self)
 	
 	#Update the compass values
 	def update(self):
-		data=bus.read_i2c_block_data(HMC5883L_ADDRESS,0)
-		compass.x=twos_comp(data[3]*256+data[4],16)
-		compass.z=twos_comp(data[5]*256+data[6],16)
-		compass.y=twos_comp(data[7]*256+data[8],16)
-		compass.heading=math.atan2(compass.y, compass.x)
-		if compass.heading <0:
-			compass.heading+=2*math.pi
-		if compass.heading >2*math.pi:
-			compass.heading-=2*math.pi
+		data = bus.read_i2c_block_data(HMC5883L_ADDRESS, 0)
+		compass.x = twos_comp(data[3]*256 + data[4], 16)
+		compass.z = twos_comp(data[5]*256 + data[6], 16)
+		compass.y = twos_comp(data[7]*256 + data[8], 16)
+		compass.heading = math.atan2(compass.y, compass.x)
+		if compass.heading < 0:
+			compass.heading += 2 * math.pi
+		if compass.heading > 2 * math.pi:
+			compass.heading -= 2 * math.pi
 		
-		compass.headingDegrees=round(math.degrees(compass.heading),2)
-		
+		compass.headingDegrees=round(math.degrees(compass.heading), 2)

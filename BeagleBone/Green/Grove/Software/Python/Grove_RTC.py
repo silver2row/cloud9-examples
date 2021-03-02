0a06aad22131798858f7bbd644e59051eb14346f
Seth <silver1row@cox.net>
2021-03-01 22:45:50 -0600

    Python3...
    Just trying to update the scripts to Python3...

--- BeagleBone/Green/Grove/Software/Python/Grove_RTC.py
+++ BeagleBone/Green/Grove/Software/Python/Grove_RTC.py
@@ -30,7 +30,9 @@
  * THE SOFTWARE.
  */
 '''
+
 #!/usr/bin/python
+
 import time
 import smbus
 
@@ -48,30 +50,30 @@ class DS1307():
 		self.SUN = 7
 		self.DS1307_I2C_ADDRESS = 0x68
 		
-		print 'begin' 
+		print ('begin') 
 		
 	def decToBcd(self, val):
-		return ( (val/10*16) + (val%10) )
+		return ( (val / 10 * 16) + (val % 10) )
 		
 	def bcdToDec(self,  val):
 		return ( (val/16*10) + (val%16) )
 		
 	def begin(self, news):
-		print news
+		print (news)
 		
 	def startClock(self):	
 		bus.write_byte(self.DS1307_I2C_ADDRESS, 0x00)
 		self.second = bus.read_byte(self.DS1307_I2C_ADDRESS) & 0x7f
 		bus.write_byte_data(self.DS1307_I2C_ADDRESS, 0x00, self.second)
 		
-		print 'startClock..'
+		print ('startClock...')
 		
 	def stopClock(self):						
 		bus.write_byte(self.DS1307_I2C_ADDRESS, 0x00)
 		self.second = bus.read_byte(self.DS1307_I2C_ADDRESS) | 0x80
 		bus.write_byte_data(self.DS1307_I2C_ADDRESS, 0x00, self.second)			
 		
-		print 'stopClock..'
+		print ('stopClock...')
 		
 	def setTime(self):
 		data = [self.decToBcd(self.second), self.decToBcd(self.minute), \
@@ -80,13 +82,13 @@ class DS1307():
 				self.decToBcd(self.year)]
 		
 		bus.write_byte(self.DS1307_I2C_ADDRESS, 0x00)
-		bus.write_i2c_block_data(self.DS1307_I2C_ADDRESS,0x00,data)
+		bus.write_i2c_block_data(self.DS1307_I2C_ADDRESS, 0x00, data)
 		
 		print 'setTime..'
 		
 	def getTime(self):
 		bus.write_byte(self.DS1307_I2C_ADDRESS, 0x00)
-		data = bus.read_i2c_block_data(self.DS1307_I2C_ADDRESS,0x00)
+		data = bus.read_i2c_block_data(self.DS1307_I2C_ADDRESS, 0x00)
 		#A few of these need masks because certain bits are control bits
 		self.second = self.bcdToDec(data[0] & 0x7f)
 		self.minute = self.bcdToDec(data[1])
@@ -96,45 +98,37 @@ class DS1307():
 		self.month = self.bcdToDec(data[5])
 		self.year = self.bcdToDec(data[6])
 		
-		print 'getTime..'
+		print ('getTime...')
 		
 	def fillByHMS(self, _hour,  _minute,  _second):
 		self.hour = _hour
 		self.minute = _minute
 		self.second = _second
 		
-		print 'fillByHMS..'
+		print ('fillByHMS...')
 		
 	def fillByYMD(self, _year,  _month,  _day):		
 		self.year = _year - 2000
 		self.month = _month;
 		self.dayOfMonth = _day
 		
-		print 'fillByYMD..'
+		print ('fillByYMD...')
 		
 	def fillDayOfWeek(self,  _dow):		
 		self.dayOfWeek = _dow
 		
-		print 'fillDayOfWeek..'
+		print ('fillDayOfWeek..')
 		
 if __name__ == "__main__": 
 	clock = DS1307()
-	clock.fillByYMD(2015,3,5)
-	clock.fillByHMS(12,42,30)
+	clock.fillByYMD(2015, 3, 5)
+	clock.fillByHMS(12, 42, 30)
 	clock.fillDayOfWeek(clock.THU)	
 	clock.setTime()
 	while True:		
 		clock.getTime()
-		print clock.hour, ":", clock.minute, ":", \
+		print (clock.hour, ":", clock.minute, ":", \
 				clock.second, " ", clock.dayOfMonth, "/", \
 				clock.month, "/", clock.year,"  ", "weekday", \
-				":", clock.dayOfWeek    		
+				":", clock.dayOfWeek)    		
 		time.sleep(1)
-		
-		
-
-	
-
-
-
-	
